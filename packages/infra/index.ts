bd7ecaae0694b5873c7cdfaed6072cdf1cc8496c
David Sauvé <dsauve@nexapp.ca>
2023-09-10 10:23:24 -0400

    add CI to deploy frontend

--- packages/infra/index.ts
+++ packages/infra/index.ts
@@ -1,9 +1,78 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
+import * as synced_folder from "@pulumi/synced-folder";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+// Create an S3 bucket and configure it as a website.
+const bucket = new aws.s3.Bucket("frontend-storage", {
+    website: {
+        indexDocument: "index.html",
+    },
+    acl: "public-read",
+});
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+// Configure ownership controls for the new S3 bucket
+const ownershipControls = new aws.s3.BucketOwnershipControls("ownership-controls", {
+    bucket: bucket.bucket,
+    rule: {
+        objectOwnership: "ObjectWriter",
+    },
+});
+
+// Configure public ACL block on the new S3 bucket
+const publicAccessBlock = new aws.s3.BucketPublicAccessBlock("public-access-block", {
+    bucket: bucket.bucket,
+    blockPublicAcls: false,
+});
+
+// Create a CloudFront CDN to distribute and cache the website.
+const cdn = new aws.cloudfront.Distribution("cdn", {
+    enabled: true,
+    origins: [{
+        originId: bucket.arn,
+        domainName: bucket.websiteEndpoint,
+        customOriginConfig: {
+            originProtocolPolicy: "http-only",
+            httpPort: 80,
+            httpsPort: 443,
+            originSslProtocols: ["TLSv1.2"],
+        },
+    }],
+    defaultCacheBehavior: {
+        targetOriginId: bucket.arn,
+        viewerProtocolPolicy: "redirect-to-https",
+        allowedMethods: [
+            "GET",
+            "HEAD",
+            "OPTIONS",
+        ],
+        cachedMethods: [
+            "GET",
+            "HEAD",
+            "OPTIONS",
+        ],
+        defaultTtl: 600,
+        maxTtl: 600,
+        minTtl: 600,
+        forwardedValues: {
+            queryString: true,
+            cookies: {
+                forward: "all",
+            },
+        },
+    },
+    priceClass: "PriceClass_100",
+    restrictions: {
+        geoRestriction: {
+            restrictionType: "none",
+        },
+    },
+    viewerCertificate: {
+        cloudfrontDefaultCertificate: true,
+    },
+});
+
+// Export the URLs and hostnames of the bucket and distribution.
+export const originURL = pulumi.interpolate`http://${bucket.websiteEndpoint}`;
+export const originHostname = bucket.websiteEndpoint;
+export const cdnURL = pulumi.interpolate`https://${cdn.domainName}`;
+export const cdnHostname = cdn.domainName;
