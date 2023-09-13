04efc70955e572380d7f3e1a296b49aabc7159ef
David Sauvé <dsauve@nexapp.ca>
2023-09-12 21:52:46 -0400

    infra: deploy backend

--- packages/infra/backend.ts
+++ packages/infra/backend.ts
@@ -1,4 +1,5 @@
 import * as aws from "@pulumi/aws";
+import {Output} from "@pulumi/pulumi";
 
 const appName = "midas-backend"
 
@@ -6,13 +7,16 @@ const appName = "midas-backend"
 const app = new aws.elasticbeanstalk.Application(appName, {});
 
 // Créer une solution Stack pour Node.js
-const solutionStackName = "64bit Amazon Linux 2018.03 v4.14.1 running Node.js";
+const solutionStackName = "64bit Amazon Linux 2023 v6.0.0 running Node.js 18";
 
-// Créer un environnement Elastic Beanstalk
-const env = new aws.elasticbeanstalk.Environment(`${appName}-env`, {
-    application: app.name,
-    solutionStackName: solutionStackName
+const midas_backend_prod = new aws.elasticbeanstalk.Environment("midas-backend-prod", {
+    application: "midas-backend",
+    name: "midas-backend-env",
+    solutionStackName: "64bit Amazon Linux 2023 v6.0.0 running Node.js 18",
+}, {
+    protect: true,
 });
 
-export const applicationName = app.name;
-export const environmentName = env.name;
+
+export const applicationName = Output.create(appName);
+export const environmentName = midas_backend_prod.name;
--- packages/infra/index.ts
+++ packages/infra/index.ts
@@ -1,11 +1,11 @@
 import {bucketName, cdnURL} from './frontend'
-import {environmentName, applicationName} from './backend'
+import {applicationName, environmentName} from './backend'
 import {addGithubVariables} from "./github";
 
 addGithubVariables({
     FRONTEND_STORAGE_S3_BUCKET_NAME: bucketName,
-    BACKEND_APPLICATION_NAME: applicationName,
-    BACKEND_ENVIRONMENT_NAME: environmentName
+    BACKEND_ENVIRONMENT_NAME: environmentName,
+    BACKEND_APPLICATION_NAME: applicationName
 })
 
 export const AppURL = cdnURL
