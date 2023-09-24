b2c651fdcddb388e12d807fefdfd11caf298f990
David Sauvé <dsauve@nexapp.ca>
2023-09-24 11:52:37 -0400

    infra: add fmp private key as github secret

--- packages/infra/github.ts
+++ packages/infra/github.ts
@@ -5,9 +5,10 @@ type GithubVariable = {
     FRONTEND_STORAGE_S3_BUCKET_NAME: Output<string>
     EC2_HOST: Output<string>
     SSH_PRIVATE_KEY: Output<string>
+    FMP_PRIVATE_KEY: Output<string>
 }
 
-export const addGithubVariables = ({FRONTEND_STORAGE_S3_BUCKET_NAME, EC2_HOST, SSH_PRIVATE_KEY}: GithubVariable) => {
+export const addGithubVariables = ({FRONTEND_STORAGE_S3_BUCKET_NAME, EC2_HOST, SSH_PRIVATE_KEY, FMP_PRIVATE_KEY}: GithubVariable) => {
     new github.ActionsVariable("FRONTEND_STORAGE_S3_BUCKET_NAME", {
         value: FRONTEND_STORAGE_S3_BUCKET_NAME.apply(v => v),
         repository: "midas",
@@ -24,5 +25,11 @@ export const addGithubVariables = ({FRONTEND_STORAGE_S3_BUCKET_NAME, EC2_HOST, S
         plaintextValue: EC2_HOST.apply(v => v),
         repository: "midas",
         secretName: "EC2_HOST"
+    });
+
+    new github.ActionsSecret("FMP_PRIVATE_KEY", {
+        plaintextValue: FMP_PRIVATE_KEY.apply(v => v),
+        repository: "midas",
+        secretName: "FMP_PRIVATE_KEY"
     });
 }
--- packages/infra/index.ts
+++ packages/infra/index.ts
@@ -1,11 +1,18 @@
 import {bucketName, cdnURL} from './frontend'
 import {publicHostName, ec2PrivateKey} from './backend'
 import {addGithubVariables} from "./github";
+import * as pulumi from "@pulumi/pulumi";
+
+const config = new pulumi.Config();
+
+const fmpPrivateKey = config.requireSecret("fmpkey")
+
 
 addGithubVariables({
     FRONTEND_STORAGE_S3_BUCKET_NAME: bucketName,
     EC2_HOST: publicHostName,
-    SSH_PRIVATE_KEY: ec2PrivateKey
+    SSH_PRIVATE_KEY: ec2PrivateKey,
+    FMP_PRIVATE_KEY: fmpPrivateKey
 })
 
 export const AppURL = cdnURL
