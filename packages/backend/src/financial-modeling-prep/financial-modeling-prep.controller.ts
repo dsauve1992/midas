4b19f6213cca4b8d00765b26011ff74ca89bd099
David Sauvé <dsauve@nexapp.ca>
2023-09-29 15:13:21 -0400

    refactor: use dedicated service FinancialModelingPrepFetcherService

--- packages/backend/src/financial-modeling-prep/financial-modeling-prep.controller.ts
+++ packages/backend/src/financial-modeling-prep/financial-modeling-prep.controller.ts
@@ -1,26 +1,14 @@
-import { Controller, Get, Param, Query, Req } from '@nestjs/common';
-import { HttpService } from '@nestjs/axios';
-import { ConfigService } from '@nestjs/config';
-import { firstValueFrom } from 'rxjs';
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { FinancialModelingPrepFetcherService } from './financial-modeling-prep-fetcher.service';
 
 @Controller('api/fmp')
 export class FinancialModelingPrepController {
   constructor(
-    private httpService: HttpService,
-    private configService: ConfigService,
+    private financialModelingPrepFetcherService: FinancialModelingPrepFetcherService,
   ) {}
 
   @Get('*')
-  async proxy(@Req() req: Request, @Param() params, @Query() query) {
-    const apiKey = this.configService.get<string>('FMP_API_KEY');
-    const url = `https://financialmodelingprep.com/api/${params[0]}`;
-
-    const queryParams = query;
-    queryParams['apikey'] = apiKey;
-
-    const { data } = await firstValueFrom(
-      await this.httpService.get(url, { params: queryParams }),
-    );
-    return data;
+  async proxy(@Param() params, @Query() query) {
+    return this.financialModelingPrepFetcherService.fetch(params[0], query);
   }
 }
--- packages/backend/src/financial-modeling-prep/financial-modeling-prep.module.ts
+++ packages/backend/src/financial-modeling-prep/financial-modeling-prep.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FinancialModelingPrepController } from './financial-modeling-prep.controller';
 import { HttpModule } from '@nestjs/axios';
+import { FinancialModelingPrepFetcherService } from './financial-modeling-prep-fetcher.service';
 
 @Module({
   controllers: [FinancialModelingPrepController],
   imports: [HttpModule],
+  providers: [FinancialModelingPrepFetcherService],
 })
 export class FinancialModelingPrepModule {}
