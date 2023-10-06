647e889c9f7cbc4adec249197b993c3183fc53ad
David Sauvé <dsauve@nexapp.ca>
2023-10-05 22:33:14 -0400

    refactor: add OwnershipService

--- packages/backend/src/ownership/ownership.module.ts
+++ packages/backend/src/ownership/ownership.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { OwnershipController } from './ownership.controller';
 import { HttpModule } from '@nestjs/axios';
+import { OwnershipService } from './ownership.service';
 
 @Module({
   controllers: [OwnershipController],
   imports: [HttpModule],
+  providers: [OwnershipService],
 })
 export class OwnershipModule {}
