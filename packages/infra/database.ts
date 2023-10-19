5a95c94c5b5e378b7566b47e23b19cd7cfcf7c1f
David Sauvé <dsauve@nexapp.ca>
2023-10-18 21:41:22 -0400

    allow dynamodb:Scan

--- packages/infra/database.ts
+++ packages/infra/database.ts
@@ -19,6 +19,7 @@ export const dynamoDbPolicy = new aws.iam.Policy("dynamoDbPolicy", {
         Statement: [{
             Effect: "Allow",
             Action: [
+                "allow dynamodb:Scan",
                 "dynamodb:GetItem",
                 "dynamodb:PutItem",
                 "dynamodb:DeleteItem",
