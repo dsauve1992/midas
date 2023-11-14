609bf38a1843959d933082e41b5b82261ac1c65e
David Sauvé <dsauve@nexapp.ca>
2023-11-13 23:04:20 -0500

    add env variables + ci

--- packages/frontend/src/ticker/ui/hooks/useTranscriptHighlightAndTakeaway.ts
+++ packages/frontend/src/ticker/ui/hooks/useTranscriptHighlightAndTakeaway.ts
@@ -1,8 +1,11 @@
 import {useQuery} from 'react-query'
 import TranscriptAPI from '../../../api/TranscriptAPI.ts'
+import {useAuth0} from "@auth0/auth0-react";
 
 export const useTranscriptHighlightAndTakeaway = (symbol: string) => {
+   const {getAccessTokenSilently} = useAuth0()
+
    return useQuery<string>(['transcript-highlight-and-takeaway', symbol], () =>
-      new TranscriptAPI().getHighLightAndTakeaway(symbol)
+      new TranscriptAPI(getAccessTokenSilently).getHighLightAndTakeaway(symbol)
    )
 }
