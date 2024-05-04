5b666c4167db870256482c97d7ff67cd77c65081
David Sauvé <david.sauve@sinistar.ca>
2024-05-04 10:53:38 -0400

    infra; add sentry monitoring

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,10 +1,12 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as fs from 'fs';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import * as http from 'http';
 import * as https from 'https';
 import * as express from 'express';
+import * as Sentry from '@sentry/node';
+import { SentryFilter } from './infra/monitoring/sentry.exception-filter';
 
 async function bootstrap() {
   const httpsOptions = {
@@ -14,12 +16,20 @@ async function bootstrap() {
     cert: fs.readFileSync('./secrets/certificate.pem'),
   };
 
+  Sentry.init({
+    environment: process.env.NODE_ENV,
+    dsn: process.env.SENTRY_DSN,
+  });
+
   const server = express();
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new SentryFilter(httpAdapter));
   app.enableCors();
   await app.init();
 
   http.createServer(server).listen(3000);
   https.createServer(httpsOptions, server).listen(443);
 }
+
 bootstrap();
