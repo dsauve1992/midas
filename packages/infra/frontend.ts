d31deee0f8d59a6a812bc829ed8994c4be4f4639
David Sauvé <david.sauve@sinistar.ca>
2024-05-06 16:56:01 -0400

    infra: update pulumi

--- packages/infra/frontend.ts
+++ packages/infra/frontend.ts
@@ -3,80 +3,82 @@ import * as aws from "@pulumi/aws";
 
 // Create an S3 bucket and configure it as a website.
 const bucket = new aws.s3.Bucket("frontend-storage", {
-    website: {
-        indexDocument: "index.html",
-        errorDocument: "index.html"
-    },
-    acl: "public-read",
+  website: {
+    indexDocument: "index.html",
+    errorDocument: "index.html",
+  },
+  acl: "public-read",
 });
 
 // Configure ownership controls for the new S3 bucket
-const ownershipControls = new aws.s3.BucketOwnershipControls("ownership-controls", {
+const ownershipControls = new aws.s3.BucketOwnershipControls(
+  "ownership-controls",
+  {
     bucket: bucket.bucket,
     rule: {
-        objectOwnership: "ObjectWriter",
+      objectOwnership: "ObjectWriter",
     },
-});
+  },
+);
 
 // Configure public ACL block on the new S3 bucket
-const publicAccessBlock = new aws.s3.BucketPublicAccessBlock("public-access-block", {
+const publicAccessBlock = new aws.s3.BucketPublicAccessBlock(
+  "public-access-block",
+  {
     bucket: bucket.bucket,
     blockPublicAcls: false,
-});
+  },
+);
 
 // Create a CloudFront CDN to distribute and cache the website.
 const cdn = new aws.cloudfront.Distribution("cdn", {
-    enabled: true,
-    customErrorResponses: [{
-        errorCode: 404,
-        responsePagePath: "/index.html",
-        responseCode: 200
-    }],
-    origins: [{
-        originId: bucket.arn,
-        domainName: bucket.websiteEndpoint,
-        customOriginConfig: {
-            originProtocolPolicy: "http-only",
-            httpPort: 80,
-            httpsPort: 443,
-            originSslProtocols: ["TLSv1.2"],
-        },
-    }],
-    defaultCacheBehavior: {
-        targetOriginId: bucket.arn,
-        viewerProtocolPolicy: "redirect-to-https",
-        allowedMethods: [
-            "GET",
-            "HEAD",
-            "OPTIONS",
-        ],
-        cachedMethods: [
-            "GET",
-            "HEAD",
-            "OPTIONS",
-        ],
-        defaultTtl: 600,
-        maxTtl: 600,
-        minTtl: 600,
-        forwardedValues: {
-            queryString: true,
-            cookies: {
-                forward: "all",
-            },
-        },
+  enabled: true,
+  customErrorResponses: [
+    {
+      errorCode: 404,
+      responsePagePath: "/index.html",
+      responseCode: 200,
     },
-    priceClass: "PriceClass_100",
-    restrictions: {
-        geoRestriction: {
-            restrictionType: "none",
-        },
+  ],
+  origins: [
+    {
+      originId: bucket.arn,
+      domainName: bucket.websiteEndpoint,
+      customOriginConfig: {
+        originProtocolPolicy: "http-only",
+        httpPort: 80,
+        httpsPort: 443,
+        originSslProtocols: ["TLSv1.2"],
+      },
     },
-    viewerCertificate: {
-        cloudfrontDefaultCertificate: true,
+  ],
+  defaultCacheBehavior: {
+    targetOriginId: bucket.arn,
+    viewerProtocolPolicy: "redirect-to-https",
+    allowedMethods: ["GET", "HEAD", "OPTIONS"],
+    cachedMethods: ["GET", "HEAD", "OPTIONS"],
+    defaultTtl: 600,
+    maxTtl: 600,
+    minTtl: 600,
+    forwardedValues: {
+      queryString: true,
+      cookies: {
+        forward: "all",
+      },
     },
+  },
+  priceClass: "PriceClass_100",
+  restrictions: {
+    geoRestriction: {
+      restrictionType: "none",
+    },
+  },
+  viewerCertificate: {
+    cloudfrontDefaultCertificate: true,
+  },
 });
 
-export const bucketName = bucket.bucket
+export const bucketName = bucket.bucket;
 
 // Export the URLs and hostnames of the bucket and distribution.
 export const originURL = pulumi.interpolate`http://${bucket.websiteEndpoint}`;
