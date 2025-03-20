b1f81832f4aa7acba67195c409c5a2753f6d5462
davidsauve <david.sauve@gphy.ca>
2025-03-20 12:25:29 -0400

    feat: create migration for position table with necessary fields and indexes

---
+++ packages/backend/migrations/20250320162051_create_position.ts
@@ -0,0 +1,27 @@
+import type { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('position', (table) => {
+    table.string('id').primary();
+    table
+      .foreign('position_wish_id')
+      .references('id')
+      .inTable('position_wishes');
+    table.string('status').notNullable();
+    table.string('user_id').notNullable();
+    table.string('symbol_with_exchange').notNullable();
+    table.integer('nb_shares', 7).notNullable();
+    table.decimal('risk_percentage', 15, 5).notNullable();
+    table.decimal('buy_price', 15, 5).notNullable();
+    table.decimal('stop_loss', 15, 5).notNullable();
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+
+    table.index('symbol_with_exchange');
+    table.index('user_id');
+    table.index('created_at');
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('position');
+}
